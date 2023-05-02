697aa260faab927a73b6e9be24d345ccb61494c9
Thiago Pereira <titiura@gmail.com>
2023-05-02 15:47:52 -0300

    feat: insigths/jobs -ok

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,55 +1,25 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, "r") as file:
+        list_job = csv.DictReader(file)
+        return list(list_job)
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    data = read(path)
+    job_type = set([job["job_type"] for job in data])
+    print(list(job_type))
+    return list(job_type)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    list_job_type = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            list_job_type.append(job)
+    return list_job_type
