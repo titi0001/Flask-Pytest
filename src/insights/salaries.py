c84c8371a44df713f947fd72aec19c54bf2f77c3
Thiago Pereira <titiura@gmail.com>
2023-05-03 18:02:13 -0300

    feat: todas as funções src/insights/salaries

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -21,48 +21,28 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        if (
+            job.get("min_salary") is None
+            or job.get("max_salary") is None
+            or min_salary > max_salary
+        ):
+            raise ValueError
+        return min_salary <= int(salary) <= max_salary
+    except Exception:
+        raise ValueError
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
-
-
+    range_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                range_salary.append(job)
+        except ValueError:
+            pass
+    return range_salary
