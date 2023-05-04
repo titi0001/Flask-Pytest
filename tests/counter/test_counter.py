05a0f3bb2e809863762a27a760fcc14792406daa
Thiago Pereira <titiura@gmail.com>
2023-05-03 21:31:43 -0300

    feat: teste de counter e sorting- ok

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    """Verifica se a função count_ocurrences esta retonando a quantidade
+    correta de palavaras"""
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+    assert count_ocurrences('data/jobs.csv', 'code') == 753
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,27 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def data():
+    return [
+        {"date_posted": "2000-01-10", "max_salary": 3000, "min_salary": 1000},
+        {"date_posted": "2000-01-11", "max_salary": 4000, "min_salary": 2000},
+        {"date_posted": "2000-01-12", "max_salary": 5000, "min_salary": 3000}
+    ]
+
+
+def test_sort_by_criteria(data):
+    sort_by(data, "min_salary")
+    assert data[1]["min_salary"] == 2000
+
+    sort_by(data, "max_salary")
+    assert data[0]["max_salary"] == 5000
+
+    sort_by(data, "date_posted")
+    assert data[0]["date_posted"] == "2000-01-12"
+
+    with pytest.raises(ValueError):
+        sort_by(data, "invalid_criteria")
+
+  
